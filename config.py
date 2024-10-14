d6c493b277f5acb4084db67e56274fc00daf701d
GRINGOLOCO7 <gorlando.ieu2022@student.ie.edu>
2024-10-14 02:03:47 +0200

    add uat

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 load_dotenv()
 
 class Config(object):
-    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'this-really-needs-to-be-changed')  # Load from env
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class LocalConfig(Config):
@@ -20,6 +20,15 @@ class LocalConfig(Config):
 class GithubCIConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
     DEBUG = True
+
+class UATConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}".format(
+        dbuser=os.getenv("DBUSER"),
+        dbpass=os.getenv("DBPASS"),
+        dbhost=os.getenv("DBHOST"),
+        dbname=os.getenv("DBNAME"),
+    )
+    DEBUG = False
 
 class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
